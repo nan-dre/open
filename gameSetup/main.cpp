cafa710d056b4e0dbcd1081e90608aca0f458332
Nandre <andu_niculae@yahoo.com>
2020-07-31 13:56:35 +0300

    Finishing touches

--- gameSetup/main.cpp
+++ gameSetup/main.cpp
@@ -29,7 +29,6 @@ BOOL WINAPI DllMain(
 
 	return true;
 }
-unsigned char mask(char type, GameMemory* memory);
 
 std::ifstream in("D:\\Proiecte\\Infoedu\\open\\gameSetup\\rom.out");
 
@@ -80,20 +79,19 @@ extern "C" __declspec(dllexport) void gameLogic(GameInput* input, GameMemory* me
 			Y = memory->r[P2];
 
 		memory->r[3] = 0;
-
-		if(input->up.pressed)
+		if(input->up.held)
 		{
 			memory->r[3] += 4;
 		}
-		if (input->down.pressed)
+		if (input->down.held)
 		{
 			memory->r[3] += 8;
 		}
-		if (input->left.pressed)
+		if (input->left.held)
 		{
 			memory->r[3] += 1;
 		}
-		if (input->right.pressed)
+		if (input->right.held)
 		{
 			memory->r[3] += 2;
 		}
@@ -178,129 +176,10 @@ extern "C" __declspec(dllexport) void gameLogic(GameInput* input, GameMemory* me
 		case 17: //bitwise and
 			memory->r[1] = bool(X & Y);
 			break;
+		case 18: //print score
+			std::cout << int(X)<<  '\n';
+			break;
 		}
-		//std::cout << "\n";//<<strlen(ram);
-		
 	}
 
-	/*char* c = (char*)volatileMemory->allocate(100);
-	char* c1 = (char*)volatileMemory->allocate(100);
-
-	c[10] = 10;
-	c1[10] = 10;*/
-	
-
-	//execute instructions
-
-	//swap buffers
-	//
-	//	buffer 1
-	//
-	//  buffer 2
-	//
-
-	// desenezi pe ecran
-
-	//for(int y=0; y<windowBuffer->h; y++)
-	//	for (int x = 0; x < windowBuffer->w; x++)
-	//	{
-	//		windowBuffer->memory[4 * (x + y * windowBuffer->w) + 0] = 0 * ((y + x)%9);	//blue
-	//		windowBuffer->memory[4 * (x + y * windowBuffer->w) + 1] = 1 * ((x + y*x ) % 17);	//green
-	//		windowBuffer->memory[4 * (x + y * windowBuffer->w) + 2] = 1 * ((x*x*x+y)%20);	//red
-	//		windowBuffer->memory[4 * (x + y * windowBuffer->w) + 3] = 0;
-	//			
-	//	}
-
-	//draw player
-	//for(int y=0; y<5; y++)
-	//	for(int x=0; x<5; x++)
-	//	{
-	//		int newX = memory->posX + x;
-	//		int newY = memory->posY + y;
-
-	//		if (newX < 0) { continue; }
-	//		if (newY < 0) { continue; }
-
-	//		if (newX >= windowBuffer->w) { continue; }
-	//		if (newY >= windowBuffer->h) { continue; }
-
-	//		windowBuffer->memory[4 * (newX + newY * windowBuffer->w) + 0] = 0;//blue
-	//		windowBuffer->memory[4 * (newX + newY * windowBuffer->w) + 1] = 255; //green
-	//		windowBuffer->memory[4 * (newX + newY * windowBuffer->w) + 2] = 255; //red
-	//	}
-
-	////move player
-
-	////float spped = 140 * deltaTime;
-	//int spped = 1;
-	//	
-	//	if(input->up.pressed)
-	//	{
-	//		memory->posY -= spped;
-	//	}
-	//	if (input->down.pressed)
-	//	{
-	//		memory->posY += spped;
-	//	}
-	//	if (input->left.pressed)
-	//	{
-	//		memory->posX -= spped;
-	//	}
-	//	if (input->right.pressed)
-	//	{
-	//		memory->posX += spped;
-	//	}
-
 }
-
-unsigned char mask(char type, GameMemory* memory)
-{
-	switch (type)
-	{
-	case 0:
-		return (memory->ram[0] >> 2);
-	case 1:
-		return (memory->ram[0] & 3);
-	case 2:
-		return (memory->ram[1] >> 6);
-	case 3:
-		return (((memory->ram[1] & 63) << 1) + (memory->ram[2] >> 7));
-	case 4:
-		return (memory->ram[2] & 127);
-	}
-	return 0;
-}
-
-//bool readEntireFile(const char* name, void* buffer, size_t size, size_t* sizeRead)
-//{
-//	if (sizeRead)
-//	{
-//		*sizeRead = 0;
-//	}
-//
-//	HANDLE file = CreateFile(name, GENERIC_READ, NULL, NULL,
-//		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-//
-//	if (file == INVALID_HANDLE_VALUE)
-//	{
-//		return 0;
-//	}
-//
-//	DWORD sizeRead1 = 0;
-//
-//	int rez = 1;
-//
-//	if (!ReadFile(file, buffer, size, &sizeRead1, NULL))
-//	{
-//		rez = 0;
-//	}
-//
-//	if (sizeRead)
-//	{
-//		*sizeRead = sizeRead1;
-//	}
-//
-//	CloseHandle(file);
-//
-//	return rez;
-//}
